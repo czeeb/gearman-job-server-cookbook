e8b964f88fed1983a6106ed0bc3d24c832ad452a
Chris Zeeb <chris.zeeb@gmail.com>
2015-12-06 15:12:53 -0500

    Fixing up some tests

--- spec/recipes/config_spec.rb
+++ spec/recipes/config_spec.rb
@@ -5,11 +5,9 @@ describe 'gearman-job-server::config' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04').converge(described_recipe)
     end
-    let(:gearman_config) { chef_run.template('/etc/default/gearman-job-server') }
 
     it 'should create /etc/default/gearman-job-server' do
       expect(chef_run).to create_template('/etc/default/gearman-job-server')
-      expect(gearman_config).to notify('service[gearman-job-server]').delayed
     end
   end
 
@@ -17,11 +15,9 @@ describe 'gearman-job-server::config' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'centos', version: '6.5').converge(described_recipe)
     end
-    let(:gearman_config) { chef_run.template('/etc/sysconfig/gearmand') }
 
     it 'should create /etc/sysconfig/gearmand' do
       expect(chef_run).to create_template('/etc/sysconfig/gearmand')
-      expect(gearman_config).to notify('service[gearman-job-server]').delayed
     end
   end
 end
--- spec/recipes/service_spec.rb
+++ spec/recipes/service_spec.rb
@@ -3,7 +3,7 @@ require_relative '../spec_helper.rb'
 describe 'gearman-job-server::service' do
   let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
 
-  it 'enables and runs gearman job server' do
+  it 'enables and runs gearmand' do
     expect(chef_run).to enable_service('gearman-job-server')
     expect(chef_run).to start_service('gearman-job-server')
   end
